fe0e58df511732094e15d86a2f1785b33c33f638
JuanAstaiza <juancarlosastaiza@gmail.com>
2021-10-29 14:22:01 -0500

    Proyectos nuevos

---
+++ suma.py
@@ -0,0 +1,15 @@
+'''
+Mi primer programa en Python
+Programa que suma dos números
+Developer: JuanAstaiza
+'''
+
+#Variables
+a=10
+b=5
+
+#Proceso
+suma=a+b
+
+#Mensaje de Salida
+print("El resultado de ",a," más ",b," es: ", suma)
